7d328dda5849ef0a555b6274f98edb6163b27e37
akuducks <kelilakou@gmail.com>
2026-01-13 16:42:43 +0100

    NUKE
    HIDE UR FILES AND FEAR FOR U LIFE NEW PROGECT STRUCTURE JUST DROPED

--- DeliveryService/DeliveryService/wsgi.py
+++ DeliveryService/DeliveryService/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DeliveryServiceApp project.
+WSGI config for DeliveryService project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeliveryServiceApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeliveryService.settings')
 
 application = get_wsgi_application()
